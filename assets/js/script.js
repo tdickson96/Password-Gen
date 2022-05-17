f5e15fc2348d471d5c8ea22dbb114c4882312cd9
Trent_D <tdickson91@gmail.com>
2022-05-17 17:02:33 -0400

    defining variables with LET

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Secure password function
+function generatePassword() {
+  let characters = prompt("Character length (minimum 8 - maximum 128");
+  let lowerCase = confirm("Toggle to use lower case letters: abcdefghijklmnopqrstuvwxyz");
+  let upperCase = confirm("Toggle to use upper case letters: ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  let numbers = confirm("Toggle to use numbers, such as: 1234567890");
+  let symbols = confirm("Toggle to use symbols, such as: !@#$%^&*()_+");
+
+  console.log(characters);
+  console.log(lowerCase);
+  console.log(upperCase);
+  console.log(numbers);
+  console.log(symbols);
+}
+
